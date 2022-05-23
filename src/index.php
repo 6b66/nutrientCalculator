4494a664b45154aa5141d19cdf1196d8584c6862
takerunpa <kenryu0618@gmail.com>
2022-05-23 20:39:02 +0900

    練習したよたける

--- src/index.php
+++ src/index.php
@@ -1,2 +1,18 @@
-<?php echo 'Hello, world' ?>
-jsfdjfsafd
+<?php
+if (isset($_POST['name'])) {
+  echo 'ようこそ!  '.$_POST['name'].'さん';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+<form method="POST" action="http://localhost:80">
+  <input type="text" name="name"/>
+</form>
+</body>
+</html>
