37ac5e3d177a5e659e862f1368c2ca19cdde2f86
6b66 <eigpbh@gmail.com>
2022-06-01 23:08:55 +0900

    テーブル作成

--- docker/mysql/DBSetUp.sql
+++ docker/mysql/DBSetUp.sql
@@ -1,1 +1,14 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH  mysql_native_password BY 'testpass'
+ALTER USER 'root'@'%' IDENTIFIED WITH  mysql_native_password BY 'testpass';
+
+CREATE DATABASE NutrientDB;
+
+CREATE TABLE NutrientDB.NutrientTable (
+    Id          INT NOT NULL PRIMARY KEY,
+    Name        BLOB NOT NULL,
+    KanaName    BLOB NOT NULL,
+    Kcalorie    FLOAT DEFAULT 0 NOT NULL,
+    Protein     FLOAT DEFAULT 0 NOT NULL,
+    Fat         FLOAT DEFAULT 0 NOT NULL,
+    Calcium     FLOAT DEFAULT 0 NOT NULL,
+    NaCL        FLOAT DEFAULT 0 NOT NULL
+);
